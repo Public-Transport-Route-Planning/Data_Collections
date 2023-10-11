75897ebaf284d3e87a60861b94a8b43a36a182ca
ninksazerac <phanchita.tj@gmail.com>
2023-10-12 04:17:50 +0700

    ⏫ upload

---
+++ station_route/scripts/bts/gold.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import os
+
+
+def generate_route(stations):
+    route_rows = []
+    for i, (code_from, station_from, lat_from, lon_from) in enumerate(stations):
+        go_row = {
+            "route_id": "BTS-G",
+            "path_type": "main",
+            "direction": "go",
+            "seq": i,
+            "sid": code_from,
+            "sname": station_from,
+            "lat": lat_from,
+            "lon": lon_from,
+        }
+        back_row = {
+            "route_id": "BTS-G",
+            "path_type": "main",
+            "direction": "back",
+            "seq": len(stations) - i - 1,
+            "sid": code_from,
+            "sname": station_from,
+            "lat": lat_from,
+            "lon": lon_from,
+        }
+
+        route_rows.extend([go_row, back_row])
+
+    return route_rows
+
+
+station_data_path = "../../../bts_station/data/bts/gold_line_station.csv"
+
+station_data = pd.read_csv(station_data_path)
+
+stations = station_data[["sid", "name_e", "lat", "lon"]].values.tolist()
+
+route_rows = generate_route(stations)
+
+route_rows = pd.DataFrame(route_rows)
+route_rows["direction"] = pd.Categorical(
+    route_rows["direction"], categories=["go", "back"], ordered=True
+)
+route_rows = route_rows.sort_values(by=["direction", "seq"])
+
+
+output_directory = "../../data/bts"
+output_filename = "gold_route.csv"  # html,csv
+
+output_path = os.path.join(output_directory, output_filename)
+route_rows.to_csv(output_path, index=False)
